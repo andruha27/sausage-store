36b189ce598c2fcc5276163831be5ce77646d250
Воробьев Андрей <a.vorobyov@city-call.ru>
2022-10-17 11:12:53 +0300

    fix

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,13 +1,3 @@
 #! /bin/bash
-#Если свалится одна из команд, рухнет и весь скрипт
 set -xe
-#Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf sausage-store.service /etc/systemd/system/sausage-store-backend.service
-sudo rm -f /var/jarservice/sausage-store.jar||true
-#Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-vorobyev-andrey-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-sudo cp ./sausage-store.jar /var/jarservice/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
-#Обновляем конфиг systemd с помощью рестарта
-sudo systemctl daemon-reload
-#Перезапускаем сервис сосисочной
-sudo systemctl restart sausage-store 
+sudo cp -rf sausage-store.service /etc/systemd/system/sausage-store-backend.service && sudo rm -f /var/jarservice/sausage-store.jar||true && curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-vorobyev-andrey-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar && sudo cp ./sausage-store.jar /var/jarservice/sausage-store.jar||true && sudo systemctl daemon-reload && sudo systemctl restart sausage-store 
