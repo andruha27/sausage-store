5ade4dcdf832593d14fcf6a08cb346838997f132
Воробьев Андрей <a.vorobyov@city-call.ru>
2022-11-13 22:33:32 +0300

    fix

--- backend/src/main/resources/db/migration/R__Add indexes.sql
+++ backend/src/main/resources/db/migration/R__Add indexes.sql
@@ -1,2 +1,2 @@
-CREATE INDEX order_id IF NOT EXISTS ON public.order_product USING btree (order_id);
-CREATE INDEX product_id IF NOT EXISTS  ON public.order_product USING btree (product_id);
+CREATE INDEX IF NOT EXISTS order_id ON public.order_product USING btree (order_id);
+CREATE INDEX IF NOT EXISTS product_id ON public.order_product USING btree (product_id);
