80a1643cdd7c37d019017e78e241669011aceb98
Воробьев Андрей <a.vorobyov@city-call.ru>
2022-11-13 22:30:37 +0300

    fix

--- backend/src/main/resources/db/migration/V02__change columns in product.sql
+++ backend/src/main/resources/db/migration/V02__change columns in product.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public.product ALTER COLUMN name TYPE varchar(30);
+ALTER TABLE public.product ALTER COLUMN name TYPE varchar(20);
 ALTER TABLE public.product ALTER COLUMN picture_url TYPE varchar(100);
