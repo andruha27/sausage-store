90a4183d6a9d40ffdb6e82eab1893b49f53d98dc
Воробьев Андрей <a.vorobyov@city-call.ru>
2022-12-11 18:44:49 +0300

    fix

---
+++ frontend/frontend_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set +e
+
+docker network create -d bridge sausage_network || true
+docker pull gitlab.praktikum-services.ru:5050/std-009-047/sausage-store/sausage-frontend:latest
+docker stop sausage-frontend || true
+docker rm sausage-frontend || true
+set -e
+docker run -d --name sausage-frontend \
+    --network=sausage_network \
+    --restart always \
+    --pull always \
+    gitlab.praktikum-services.ru:5050/std-009-047/sausage-store/sausage-frontend:latest
