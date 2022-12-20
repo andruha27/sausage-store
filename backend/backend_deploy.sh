a35a44130aba76b9adc965fcc0d751e58942ee36
Воробьев Андрей <a.vorobyov@city-call.ru>
2022-12-21 01:49:11 +0300

    fix

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -7,8 +7,6 @@ SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:27018/${MONGO_DATABASE}?tls=true&replicaSet=rs01
 EOF
 
-cat .env
-
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-009-047/sausage-store/sausage-backend:latest
 docker stop sausage-backend || true
