6d6ba3186d68863ca46c4321b50e18cca1fe06b5
Воробьев Андрей <a.vorobyov@city-call.ru>
2023-01-22 19:57:08 +0300

    fix

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -5,10 +5,9 @@ DB=mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:27018/${MONGO_DATABAS
 PORT=8080
 EOF
 
-docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-009-047/sausage-store/sausage-report:latest
-docker stop sausage-backend-report || true
-docker rm sausage-backend-report || true
+docker stop backend-report || true
+docker rm backend-report || true
 
 set -e
 docker-compose --env-file .backend.report.env up -d backend-report
--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -2,9 +2,8 @@
 set +e
 
 
-docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-009-047/sausage-store/sausage-frontend:latest
-docker stop sausage-frontend || true
-docker rm sausage-frontend || true
+docker stop frontend || true
+docker rm frontend || true
 set -e
 docker-compose up -d frontend
