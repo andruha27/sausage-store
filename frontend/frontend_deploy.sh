80f631f81cf402c07185c8d98e53616ed5a3976b
Воробьев Андрей <a.vorobyov@city-call.ru>
2023-01-22 18:47:54 +0300

    fix

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set +e
 
+cat > .frontend.env <<EOF
+
+EOF
+
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-009-047/sausage-store/sausage-frontend:latest
 docker stop sausage-frontend || true
 docker rm sausage-frontend || true
 set -e
-docker-compose up -d frontend-report
+docker-compose up --env-file .frontend.env -d frontend
